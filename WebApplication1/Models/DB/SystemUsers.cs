27065257a8efdafa7a8b5f57faeb3a4238e45fb3
rbmemarion <rbmemarion@student.apc.edu.ph>
2023-09-09 22:19:17 +0800

    Add project files.

---
+++ WebApplication1/Models/DB/SystemUsers.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.DB
+{
+    public class SystemUsers
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string LogInName { get; set; }
+        public string PasswordEncryptedText { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ModifiedDateTime { get; set; }
+    }
+}
